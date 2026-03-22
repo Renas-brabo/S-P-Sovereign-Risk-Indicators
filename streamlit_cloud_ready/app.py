948a901314b96ac92f6dc8302cea14128bf9c2bc
Renato Lourenço <61302970+Renas-brabo@users.noreply.github.com>
2026-03-21 22:41:14 -0300

    Update title and caption for S&P Dashboard

--- streamlit_cloud_ready/app.py
+++ streamlit_cloud_ready/app.py
@@ -388,8 +388,8 @@ def render_table_tab(df: pd.DataFrame):
 
 
 def main():
-    st.title("📊 BDA Dashboard")
-    st.caption("Aplicação Streamlit pronta para deploy no GitHub + Streamlit Cloud.")
+    st.title("📊 S&P Sovereign Risk Indicators Data")
+    st.caption("Dados Públicos da base da agência S&P")
 
     local_file = find_local_xlsx()
     with st.expander("Arquivo de entrada", expanded=False):
